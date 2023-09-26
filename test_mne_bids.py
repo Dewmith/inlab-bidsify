5d2e6d1e7e6e6d4be26866a1be1519dff6f8ed25
Anne-Sophie Dubarry <anne-sophie.dubarry@univ-amu.fr>
2023-09-26 17:01:13 +0200

    Initial commit de la branche develop (with gitignore)

--- test_mne_bids.py
+++ test_mne_bids.py
@@ -3,21 +3,19 @@
 """
 Created on Thu Feb 10 13:35:52 2022
 
-@author: Simon More, A.-Sophie Dubarry
+@author: simon
 """
 
 import mne
 from mne_bids import BIDSPath, write_raw_bids
+edf_path="/media/simon/DATADRIVE2/openscience/requetes/eeg_recording/ma0844az_1-1+.edf"
 
-raw_data="/Users/annesophiedubarry/Documents/0_projects/in_progress/DISC/ADOEFFORT_mgrosbras/data/sub00/sub00-nback.bdf"
-out_path="/Users/annesophiedubarry/Documents/0_projects/in_progress/DISC/ADOEFFORT_mgrosbras/data/bids_datasets"
+raw = mne.io.read_raw_edf(edf_path, preload=False)
 
-raw = mne.io.read_raw_bdf(raw_data, preload=False)
+task='simon'
 
-task='nback'
-
-bids_path = BIDSPath(subject='s00', session='nback', run='1',
-                         datatype='eeg', task=task, root=out_path)
+bids_path = BIDSPath(subject='66', session='077', run='99',
+                         datatype='eeg', task=task, root='./bids_dataset')
 write_raw_bids(raw, bids_path=bids_path)
 
-print("BIDS data created at "+out_path)
+print("OK")
