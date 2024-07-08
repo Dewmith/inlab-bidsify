69bad0f7d4af39e785b6b412985b6e616e75556f
ArnaudWeill <arnaud.weill@univ-amu.fr>
2024-07-08 14:36:02 +0200

    Update test_mne_bids.py

--- test_mne_bids.py
+++ test_mne_bids.py
@@ -6,11 +6,16 @@ Created on Thu Feb 10 13:35:52 2022
 @author: Simon More, A.-Sophie Dubarry
 """
 
+import os
 import mne
 from mne_bids import BIDSPath, write_raw_bids
 
-raw_data="/Users/annesophiedubarry/Documents/0_projects/in_progress/DISC/ADOEFFORT_mgrosbras/data/sub00/sub00-nback.bdf"
-out_path="/Users/annesophiedubarry/Documents/0_projects/in_progress/DISC/ADOEFFORT_mgrosbras/data/bids_datasets"
+# raw_data="/Users/annesophiedubarry/Documents/0_projects/in_progress/DISC/ADOEFFORT_mgrosbras/data/sub00/sub00-nback.bdf"
+# out_path="/Users/annesophiedubarry/Documents/0_projects/in_progress/DISC/ADOEFFORT_mgrosbras/data/bids_datasets"
+
+user_dir = os.path.expanduser('~')
+raw_data= os.path.join(user_dir, 'Documents', 'Python-Scripts' ,'BIDSIF', 'Entree_Q0', 'sub03', 'sub03-proactive-test.bdf')
+out_path= os.path.join(user_dir, 'Documents', 'Python-Scripts' ,'BIDSIF', 'Sortie_Q0')
 
 raw = mne.io.read_raw_bdf(raw_data, preload=False)
 
